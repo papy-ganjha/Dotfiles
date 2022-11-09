3a7460b3e24b90c9acd00f9bb2beee025bba44fa
papy-ganja <kenz.bensmaine@gmail.com>
2022-11-09 21:15:53 +0100

    Move nvim config

---
+++ install.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+echo This script will install some packages that are importants
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
