1960c67e8745623187b8cdb783456e34819d06d8
ganjaman <kenz.bensmaine@gmail.com>
2023-01-06 21:15:02 +0100

    C'est bvon

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,6 +4,12 @@ sudo apt update && sudo apt upgrade -y
 # Install python and deps
 sudo apt install python3.8 python3.8-venv python3.8-dev
 
+# Install docker and configure without sudo
+sudo apt install docker docker.io docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 # Install CUDA and CUDNN
 ./setup-cuda-ubuntu.sh
 
@@ -16,3 +22,10 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release
 .list
 sudo apt update && sudo apt install brave-browser
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# copy the dot files to the root folder
+ln -s .* ~/
