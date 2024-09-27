96c5aedfb1d638ad6963d234b4eec6f9f1ead43d
Kenz <kenz@Mac.attlocal.net>
2024-09-27 10:48:19 -0700

    More themes and better script for installing tools

--- .config/nvim/lua/kenzb/core/colorscheme.lua
+++ .config/nvim/lua/kenzb/core/colorscheme.lua
@@ -1,5 +1,5 @@
 -- Minimal config
 require("themery").setup({
-	themes = { "gruvbox", "catppuccin" }, -- Your list of installed colorschemes.
+	themes = { "gruvbox", "catppuccin", "tokyonight" }, -- Your list of installed colorschemes.
 	livePreview = true, -- Apply theme while picking. Default to true.
 })
--- scripts/install_tmux_nvim_config.sh
+++ scripts/install_tmux_nvim_config.sh
@@ -1,3 +1,6 @@
+# Install neovim needs for all packages
+brew install rg fzf npm
+
 #TPM Plugin manager for tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
