b1d749720168c2b1633536e00fb9d225a0783203
kenz <k_bensmaine@apple.com>
2025-07-17 17:34:18 -0700

    New preview plugin

--- .config/nvim/lua/kenzb/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/kenzb/plugins/lsp/lspconfig.lua
@@ -12,13 +12,9 @@ end
 
 local keymap = vim.keymap -- for conciseness
 
-
-local opts = { noremap = true, silent = true }
-
--- set keybinds
 keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
 keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- see definition and make edits in window
+keymap.set("n", "gd", require('goto-preview').goto_preview_definition, opts)
 keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
 keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
