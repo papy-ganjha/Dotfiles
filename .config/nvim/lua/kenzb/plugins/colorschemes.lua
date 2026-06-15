4ecf303afe4678d90fc64869be13b165e4261fbb
kenz <k_bensmaine@apple.com>
2026-06-15 16:48:15 -0700

    Use autoatic theme toggler

--- .config/nvim/lua/kenzb/plugins/colorschemes.lua
+++ .config/nvim/lua/kenzb/plugins/colorschemes.lua
@@ -1,33 +1,40 @@
 return {
-  -- Theme selector
+  -- Theme selector (configured in core/colorscheme.lua)
   {
     "zaldih/themery.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("themery").setup({
-        themes = { "gruvbox", "catppuccin", "tokyonight" },
-        livePreview = true,
-      })
-    end,
   },
 
-  -- Catppuccin theme
+  -- Auto-switch light/dark with macOS appearance
+  {
+    "f-person/auto-dark-mode.nvim",
+    lazy = false,
+    priority = 1001,
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd.colorscheme("catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd.colorscheme("catppuccin-latte")
+      end,
+    },
+  },
+
   {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
   },
-
-  -- Tokyonight theme
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
   },
-
-  -- Gruvbox theme
   {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
