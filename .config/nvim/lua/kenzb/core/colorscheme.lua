c1df856c3ce8236d1e8203e0d0a0f3b1d449ddd7
kenz <k_bensmaine@apple.com>
2026-06-15 16:21:50 -0700

    Updates of tmux for performances and some colorscheme better no hardocing

--- .config/nvim/lua/kenzb/core/colorscheme.lua
+++ .config/nvim/lua/kenzb/core/colorscheme.lua
@@ -1,5 +1,5 @@
 -- Minimal config
 require("themery").setup({
-	themes = { "gruvbox", "catppuccin", "tokyonight" }, -- Your list of installed colorschemes.
-	livePreview = true, -- Apply theme while picking. Default to true.
+	themes = vim.fn.getcompletion("", "color"),
+	livePreview = true,
 })
